0157942d7b279a396ad8c06d848094f2dec84ff6
Kay <118884667+BadRix90@users.noreply.github.com>
2026-01-26 22:11:23 +0100

    Migrate to Whisper + Gemini 2.5 Flash hybrid approach

--- quizzes/services/__init__.py
+++ quizzes/services/__init__.py
@@ -1,7 +1,7 @@
+# quizzes/services/__init__.py
 """Quiz services - YouTube download, transcription and quiz generation."""
 
 from .youtube_service import download_audio
-from .gemini_service import generate_quiz
-from ..utils.quiz_generator import transcribe_audio
+from ..utils.quiz_generator import transcribe_audio, generate_quiz
 
 __all__ = ['download_audio', 'transcribe_audio', 'generate_quiz']
